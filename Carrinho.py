70cc9737011f13f34f40830869db6d18b051ae05
Karine Rodrigues <karinemartinsro@gmail.com>
2026-05-04 21:35:21 -0300

    Implementação deletar item

--- Carrinho.py
+++ Carrinho.py
@@ -18,3 +18,30 @@ class CarrinhoDeCompras:
 
         print(f"TOTAL Á PAGAR : R$ {total}")
         print("-" *45)
+
+    def listar_produtos(self, produto):
+        if not produto:
+            print("O carrinho está vazio !")
+            return
+        
+        for i, item in enumerate (self.itens):
+
+    
+    
+    
+    def deletar_produto(self, produto):
+        self.itens
+        if not self.itens:
+            print("O carrinho está vazio !")
+            return
+        print("Selecione o item para excluir :")
+        for i, item in enumerate (self.itens):
+            opcao = item['Opção']
+
+        try:
+            indice = int(input("Digite o número do item que deseja excluir do carrinho?"))
+            item_removido = self.itens.pop(indice)
+            print("Feito, item deletado do carrinho !")
+
+        except:
+            print("Digite uma opção válida . ")
